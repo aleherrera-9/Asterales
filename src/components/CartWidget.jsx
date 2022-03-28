fdfd9a89c1f11687b4ec3576bd6481e1ab75a9f2
aleherrera-9 <aleherrera.9.m@gmail.com>
2022-03-28 13:37:05 -0300

    IconUpdated

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,9 +2,16 @@ import { BsCart3 } from "react-icons/bs";
 import { FaRegUser } from "react-icons/fa";
 const CartWidget= () => {
     return(
-        <div className="text-white fs-4 text-end m-3">
-            <FaRegUser/> |
-            5 <BsCart3 />
+        <div className="text-white fs-4 text-end m-3 ">
+             <ul className="nav justify-content-end">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#" ><FaRegUser/></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active bg black" aria-current="page" href="#" >5 <BsCart3 /></a>
+                </li>
+           </ul> 
+         
         </div>
     )
 }
